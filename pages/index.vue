1de5b45bc76dbca5b7ff51720df58f028db633d6
Alwaysblues <parinyadahmi@hitmail.com>
2019-10-30 00:33:48 +0700

    init

---
+++ pages/index.vue
@@ -0,0 +1,327 @@
+<template>
+
+  <div>
+
+
+    <div class="hero">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+        <div class="container">
+
+          <a class="navbar-brand" href="#"> LOGO </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Service</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="#">Project</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="#">Team</a>
+              </li>
+
+
+              <li class="nav-item">
+                <a class="nav-link" href="#">Expertise</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="#">Get in touch</a>
+              </li>
+
+
+            </ul>
+
+          </div>
+        </div>
+      </nav>
+
+
+      <div class="container">
+        <div class="row align-items-center" style="min-height: 100vh">
+          <div class="col">
+
+            <div class="mb-5">
+              <h1>Better Institutions,</h1>
+              <h1>Better Outcome.</h1>
+              <div>
+                In a rapidly changing the world, we have a expertise to help our clients keep up.
+              </div>
+            </div>
+
+            <button type="button" class="btn btn-dark">SEE OUR SERVICE</button>
+
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+
+
+    <div class="container">
+
+      <!-- project -->
+      <div class="select">
+        <h1>Explore our lasted project</h1>
+        <div class="mb-5">But I must explain to you how all this mistaken idea
+          of denouncing pleasure and praising pain was born.
+        </div>
+
+        <div class="row">
+          <div class="col-12 col-sm-6">
+            <img class="img-fluid" src="../assets/image/service.png">
+          </div>
+          <div class="col-12 col-sm-6">
+            <img class="img-fluid" src="../assets/image/service.png">
+          </div>
+        </div>
+        <button type="button" class="btn btn-dark">VIEW ALL PROJECTS</button>
+      </div>
+      <!-- end project -->
+
+
+      <!-- about -->
+      <div class="select">
+        <div class="row">
+          <div class="col-12 col-sm-4">
+            <h1>About Us</h1>
+          </div>
+
+          <div class="col-12 col-sm-8">
+            <div class="mb-3">
+              International Financial Consulting is a boutique firm of global experts working with development finance
+              institutions around the world on a wide range of challenges, from strategy and governance to the design
+              and
+              marketing of financial instruments. We combine senior expertise with data-driven insights and customized
+              advisory services to help our clients realize their full potential as catalysts for development, trade,
+              and
+              economic growth.
+            </div>
+
+            <button type="button" class="btn btn-dark">LEARN MORE</button>
+          </div>
+        </div>
+      </div>
+      <!-- end about -->
+
+
+      <!-- team -->
+      <div class="select">
+        <div class="row align-items-center">
+
+          <div class="col-12 col-sm-6">
+            <img class="img-fluid" src="../assets/image/team.png">
+          </div>
+
+          <div class="col-12 col-sm-6">
+            <h1>Our people, our advantage</h1>
+            <div>
+              Our team of international experts are why clients recommend us. We combine unparalleled expertise and
+              thought leadership on trade and development finance with practical hands-on experience.
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <!-- end team -->
+
+
+      <!-- service -->
+      <div class="select">
+        <div class="row align-items-center">
+
+
+          <div class="col-12 col-sm-6">
+            <h1>Services</h1>
+
+            <div class="mb-3">
+              We know what it takes to run successful public-policy based institutions. Our core competency is in
+              institutional development, including strategic planning, evaluation, policy formulation and programming,
+              product innovation, governance systems and risk management.
+            </div>
+
+            <button type="button" class="btn btn-dark">OUR SERVICES</button>
+
+          </div>
+
+
+          <div class="col-12 col-sm-6">
+            <img class="img-fluid" src="../assets/image/service.png">
+          </div>
+
+        </div>
+      </div>
+      <!-- end service -->
+
+
+      <!-- expertise -->
+      <div class="select">
+        <div class="row align-items-center">
+
+          <div class="col-12 col-sm-6">
+            <img class="img-fluid" src="../assets/image/expertise.png">
+          </div>
+
+          <div class="col-12 col-sm-6">
+            <h1>Expertise</h1>
+            <div class="mb-3">
+              We have extensive experience across all areas of development finance, including trade, export and
+              investment, development finance and climate finance. Our expert team brings a unique combination of deep
+              knowledge and expert perspectives to our client's challenges,
+              offering fresh ideas and breakthrough insights.
+            </div>
+
+            <div class="mb-3">
+              <small>
+                If you are in the market for a computer,<br> there are a number of factors to consider.
+              </small>
+            </div>
+
+            <div>
+              <button type="button" class="btn btn-dark">OUR EXPERTISE</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- end expertise -->
+    </div>
+
+
+    <!-- footer -->
+    <div class="footer" style="background: #7d7d7d">
+
+      <div class="container select">
+
+        <div class="row">
+          <div class="col-12 col-sm-6">
+            <h1> Get in touch </h1>
+            <div class="mb-3">
+              We help organizations with their most critical challenges and opportunities. Contact us now to learn how
+              we can work together.
+            </div>
+            <button type="button" class="btn btn-dark">CONTACT</button>
+          </div>
+
+          <div class="col-12 col-sm-6">
+            <h1> Work with Us </h1>
+            <div class="mb-3">
+              We're on a mission to change the world. Learn how you can join a team focused on providing top tier advice
+              and innovative, but workable solutions to our clients.
+            </div>
+            <button type="button" class="btn btn-dark">CAREERS</button>
+          </div>
+        </div>
+      </div>
+
+
+      <hr>
+
+      <div class="container select">
+        <div class="row mb-5">
+          <div class="col-12 col-sm-3"> LOGO</div>
+          <div class="col-12 col-sm-3">
+            <div class="mb-3">
+              <h5>
+                PHONE
+              </h5>
+
+              <div>
+                +1 123 345 2234
+              </div>
+            </div>
+            <div>
+
+              <h5>
+                EMAIL
+              </h5>
+
+              <div>
+                info@zed.xyz
+              </div>
+
+            </div>
+          </div>
+          <div class="col-12 col-sm-3">
+            <div>
+              <h5>
+                SOCIAL
+              </h5>
+
+              <div>
+                Linkedin
+              </div>
+              <div>
+                Facebook
+              </div>
+              <div>
+                Twitter
+              </div>
+
+            </div>
+          </div>
+          <div class="col-12 col-sm-3">
+
+            <div>
+              <h5>
+                MAILING ADDRESS
+              </h5>
+
+              <div>
+                Zed Financial Consulting Ltd.
+                World Exchange Plaza
+                P.O. Box 811219
+                Ottawa, Ontario. K1P 1B1
+                Canada
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+
+
+        <div>
+          © 2019 Zed Financial Consulting LTD. Privacy Policy.
+        </div>
+
+      </div>
+    </div>
+    <!-- end footer -->
+
+
+  </div>
+</template>
+
+<script>
+  import Logo from '~/components/Logo.vue'
+
+  export default {
+    components: {
+      Logo
+    }
+  }
+</script>
+
+<style>
+
+  .hero {
+    background-image: url("../assets/image/hero_bg.png");
+    background-position: center;
+    background-size: cover;
+    min-height: calc(100vh);
+  }
+
+</style>
